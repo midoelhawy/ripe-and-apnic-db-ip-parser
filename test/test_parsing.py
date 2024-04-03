883758f3af996e3baa1ecb4800f76c91e71864ae
Ahmed Hekal <ahekal@movia.biz>
2024-04-03 17:45:36 +0200

    dev:  test parser calback

---
+++ test/test_parsing.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import sys
+path_root = Path(__file__).parents[1]
+sys.path.append(str(path_root))
+print(sys.path)
+
+from lib.ripe_parser import RIPE_PARSER
+
+# file_path = str(Path.joinpath(Path(__file__).parents[1],'test/chunk.db'))
+# file_path = "/home/ahmedhekal/work/personal/ripe-ip-parser/db/chunks/0/0.db"
+file_path = "/home/ahmedhekal/work/personal/ripe-ip-parser/db/ripe.db.inetnum"
+
+def on_single_block_process(block):
+    print(block)
+
+parse_data = RIPE_PARSER.parse_file(file_path,on_single_block_process)
+print(parse_data)
